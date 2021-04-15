ac77e22cab6231f61aea6c1d3511c813e1acf1b5
likithchandra <82393093+likithchandra@users.noreply.github.com>
2021-04-15 12:32:53 +0530

    Add files via upload

---
+++ .CED17I041_capstrokescapture.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/likith/devicedrivers/lab9/CED17I041_capstrokescapture.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000 -T ./scripts/module-common.lds  --build-id  -o /home/likith/devicedrivers/lab9/CED17I041_capstrokescapture.ko /home/likith/devicedrivers/lab9/CED17I041_capstrokescapture.o /home/likith/devicedrivers/lab9/CED17I041_capstrokescapture.mod.o ;  true
